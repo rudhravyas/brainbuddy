791ddc2519a37f3e8f75cfe802652c0c856d735c
rudhravyas <91895312+rudhravyas@users.noreply.github.com>
2024-06-24 09:58:32 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import ContextProvider from './Context/Context'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+ 
+
+  
+  
+)
+ 
